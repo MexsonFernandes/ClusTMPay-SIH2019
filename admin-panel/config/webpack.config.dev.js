3432b23833be81ed4785028364cb6905969ee542
ANSH VARUN <30474923+anshcena@users.noreply.github.com>
2019-02-25 00:45:37 +0530

    Delete webpack.config.dev.js

--- admin-panel/config/webpack.config.dev.js
+++
@@ -1,9 +0,0 @@
-var BundleTracker  = require('webpack-bundle-tracker');
-//...
-
-module.exports = {
-
-    plugins: [
-          new BundleTracker({path: "../", filename: 'webpack-stats.json'}),
-    ]
-}
