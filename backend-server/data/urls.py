bdcbd37c775fe8cd80eb443641c77c41001bbc03
Mexson Fernandes <robomex2020@gmail.com>
2019-03-01 22:15:20 +0530

    (ADDED) firebase

--- backend-server/data/urls.py
+++ backend-server/data/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from data import views
 
 urlpatterns = [
+    path('send_note', views.send_note),
     path('send_note_all', views.send_note_all),
     path('save_firebase_data', views.firebase)
 ]
